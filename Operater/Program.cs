3af869aa74ed300bcabb0727ba3c978d268e526d
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-07-07 21:10:27 +0700

    add

---
+++ Operater/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        float width;
+        float height;
+        Console.WriteLine("Nhap gia tri cua chieu rong: ");
+        width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap gia tri cua chieu dai: ");
+        height = Convert.ToInt32(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine($"gia tri cua dien tich la: {area}");
+    }
+}
