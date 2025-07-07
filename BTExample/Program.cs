f1f68e5aa92a03ec16afa368a99817bab99dfec2
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-07-07 21:03:35 +0700

    add

---
+++ BTExample/Program.cs
@@ -0,0 +1,9 @@
+﻿public class MyClass
+{
+    public static void Main(string[] args)
+    {
+        DateTime dateTime = DateTime.Now;
+        
+        Console.WriteLine("" + dateTime.Hour + " " + dateTime.Minute + " " + dateTime.Second);
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //     Console.WriteLine("Hello, World!");
+            //     Console.Write("Hay nhap mot so tren bàn phím: ");
+            //     int so = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("Số vừa nhập " + so);
+            Console.Write("Hay nhap bán kính hình tròn: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double s = Math.PI * r * r;
+            Console.WriteLine("Bán kính là: " + s);
+        }
+    }
+}
