aa6ce6cbebb9d186cacb0154a497cf87be64d209
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-07-21 09:29:08 +0700

    add money change

---
+++ ExampleMoneyChange/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Nhap so tien USD: ");
+        int moneyVND = 23000;
+        int moneyUSD = Convert.ToInt32(Console.ReadLine());
+        int change = moneyUSD * moneyVND;
+        Console.WriteLine("So tien VND la: " + change);
+    }    
+}
