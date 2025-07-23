2da635c9274b43c5905a6a2a8d058cbab9192065
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-07-23 14:43:33 +0700

    add calculate bmi

---
+++ ExampleCalculateBMI/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap can nang: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap chieu cao: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        double bmi = weight / Math.Pow(height, 2);
+        bmi = Math.Round(bmi,1);
+        Console.Write("BMI la " + bmi);
+
+        if (bmi < 18)
+        {
+            Console.WriteLine(" Thieu can");
+        }
+        else if (bmi < 25.0)
+        {
+            Console.WriteLine(" Binh thuong");
+        }
+        else if (bmi < 30.0)
+        {
+            Console.WriteLine(" Thua can");
+        }
+        else
+        {
+            Console.WriteLine(" Beo phi");
+        }
+    }   
+}
