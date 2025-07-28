9041dcc9a3ebc7b868efa69db8b6e18141305e6c
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-07-28 12:35:05 +0700

    add example array two find max

---
+++ ExampleArrayTwoFindMax/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] matrix = new int[4, 3]
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 },
+            { 10, 11, 12 }
+        };
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+        }
+
+        int max = matrix[0, 0];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i,j] > max)
+                {
+                    max = matrix[i, j];
+                }
+                
+            }
+        }
+        Console.WriteLine("Max cua mang la " + max);
+    }   
+}
