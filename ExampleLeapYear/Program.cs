635adeac588fa4b98f1ea2ff5b74f8eba0730ae6
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-07-28 12:01:59 +0700

    add example leap year

---
+++ ExampleLeapYear/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap vao nam: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+        bool isLeapYear = false;
+
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    isLeapYear = true;
+                }
+            }
+            else
+            {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear)
+        {
+            Console.WriteLine($"{year} la nam nhuan");
+        }
+        else
+        {
+            Console.WriteLine($"{year} khong phai la nam nhuan");
+        }
+    }    
+}
