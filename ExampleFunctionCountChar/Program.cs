34d8afcaf7254495fb257e05bf9506d4a5137990
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-08 16:02:32 +0700

    example function count char

---
+++ ExampleFunctionCountChar/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static int CountChar(string str, char c)
+    {
+        int count = 0;
+        foreach (char ch in str)
+        {
+            if (ch == c)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chuoi:");
+        string str = Console.ReadLine();
+        Console.WriteLine("Nhap ky tu can dem: ");
+        char c = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        int count = CountChar(str, c);
+        Console.WriteLine($"So lan xuat hien cua '{c}' trong chuoi '{str}' la: {count}");
+        
+    }    
+}
