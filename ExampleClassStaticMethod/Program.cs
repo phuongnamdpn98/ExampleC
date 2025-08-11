5bb32da272209254b4e8e50bbdc20569c52dfc5b
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-11 19:55:37 +0700

    add example class static method

---
+++ ExampleClassStaticMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿
+public class Student
+{
+    private int rollNo;
+    private string name;
+    private static string college = "BBDIT";
+
+    public Student(int r, string n)
+    {
+        rollNo = r;
+        name = n;
+    }
+
+    public static void Change()
+    {
+        college = "CODEGYM";
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(rollNo + " " + name + " " + college);
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student.Change();
+        Student s1 = new Student(111, "Hoang");
+        Student s2 = new Student(222, "Khanh");
+        Student s3 = new Student(333, "Nam");
+
+        s1.Display();
+        s2.Display();
+        s3.Display();
+    }
+}
