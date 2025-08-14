637f32adc70d2cc5b2c8556301050c132ac80bf5
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-14 16:47:59 +0700

    add example interface IColorable

---
+++ ExampleInterfaceIColorable/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        object[] shapes =
+        {
+            new Circle(5),
+            new Rectangle(4, 6),
+            new Square(7)
+        };
+
+        foreach (var shape in shapes)
+        {
+            if (shape is IColorable colorable)
+            {
+                colorable.HowToColor();
+            }
+        }
+    }    
+}
