028f76401b37c33d4c551bfbfb9f10a4fc1359fc
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-14 14:44:11 +0700

    add example circle comparer

---
+++ ExampleInterfaceIComparer/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[2] = new Circle(3.5, "indigo", false);
+
+        Console.WriteLine("Pre-sorted:");
+        foreach (Circle circle in circles)
+        {
+            Console.WriteLine(circle);
+        }
+
+        IComparer<Circle> circleComparator = new CircleComparator();
+        Array.Sort(circles, circleComparator);
+
+        Console.WriteLine("After-sorted:");
+        foreach (Circle circle in circles)
+        {
+            Console.WriteLine(circle);
+        }
+    }
+}
