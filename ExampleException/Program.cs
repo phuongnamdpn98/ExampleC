61f39cbafe7a340c65422649f36849f77ee27898
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-17 13:26:37 +0700

    add example exception
    add example exception

---
+++ ExampleException/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            // Console.WriteLine("Nhap gia tri so nguyen a: ");
+            // int a = Convert.ToInt16(Console.ReadLine());
+            // Console.WriteLine("Nhap gia tri so nguyen b: ");
+            // int b = Convert.ToInt16(Console.ReadLine());
+            // Console.WriteLine($"Thuong la {a / b}");
+
+            int x = int.Parse("abc");
+        }
+        catch (FormatException e)
+        {
+            //Console.WriteLine("Ban da nhap so 0");
+            Console.WriteLine($"exception {e}");
+        }
+
+        try
+        {
+            int[] arr = new int[12];
+            Console.WriteLine(arr[13]);
+        }
+        catch (IndexOutOfRangeException e)
+        {
+
+            Console.WriteLine($"exception out range {e}");
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine($"exception null {e}");
+
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine($"exception out range {e}");
+        }
+        finally
+        {
+            Console.WriteLine("ket thuc");
+        }
+    }
+}
---
+++ ExampleIndexOutOfRangeException/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+                int value = characters[6];
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex);
+            }
+    }    
+}
