b3df5d98b494655762fd65a985957149bf5d6ef7
Dang Nam <116860706+phuongnamdpn98@users.noreply.github.com>
2025-08-20 13:52:35 +0700

    add example read text file

---
+++ ExampleFileInfor/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // string sourceFilePath = "D:\\Unity\\Source.txt";
+        // string destinationFilePath = "D:\\Unity\\Destination.txt";
+        // FileInfo sourceFile = new FileInfo(sourceFilePath);
+        // sourceFile.CopyTo(destinationFilePath, true);
+        // long fileSize = sourceFile.Length;
+        // Console.WriteLine($"File copied successfully Size {fileSize}");
+        // bool doesFileExist = File.Exists(destinationFilePath);
+        // File.Copy(sourceFilePath, destinationFilePath, true);
+        // Console.WriteLine($"File copy is {sourceFilePath} to {destinationFilePath} File exist: {doesFileExist}");
+
+        string filePath = "D:\\Unity\\FileNew";
+        //Directory.CreateDirectory(filePath);
+        DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+        directoryInfo.Create();
+    }    
+}
---
+++ ExampleReadTextFile/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    public static void ReadTextFile(string filePath)
+    {
+        try
+        {
+            FileInfo fileInfo = new FileInfo(filePath);
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException($"The file '{filePath}' does not exist.");
+            }
+            StreamReader reader = new StreamReader(fileInfo.FullName);
+            string line = "";
+            int sum = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+                sum += Int32.Parse(line);
+            }
+            reader.Close();
+            Console.WriteLine("Total Sum: " + sum);
+        }
+        catch (System.Exception)
+        {
+
+            Console.Error.WriteLine("File not found or invalid content");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please input file path");
+        string path = Console.ReadLine();
+        Console.WriteLine("File path: " + path);
+        ReadTextFile(path);
+    }
+}
